067df915ed4a2cf2c37ab849df3d11231211dfc0
reymero <57924221+reymero@users.noreply.github.com>
2020-02-07 17:19:24 -0500

    Add files via upload
    a shopping cart built using object-oriented programming.

---
+++ payment.php
@@ -0,0 +1,10 @@
+<?php 
+
+
+$PayPalMode 			= 'sandbox'; // sandbox or live
+$PayPalApiUsername 		= 'account@gmail.com'; //PayPal API Username
+$PayPalApiPassword 		= '979797979'; //Paypal API password
+$PayPalApiSignature 	= 'AewouidSeoiewDradoZcgqH3hpacAokIiuNjAwoiedkew'; //Paypal API Signature
+$PayPalCurrencyCode 	= 'USD'; //Paypal Currency Code
+$PayPalReturnURL 		=  //Point to paypal-express-checkout page
+$PayPalCancelURL 		= 
